1a055f306a83e9638c4cae8cca3fb98e6ad10ecb
HD162630 <eclair0111@naver.com>
2017-06-02 09:24:39 +0900

    자바 수행 과제

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -19,13 +19,12 @@ public class MovieTest {
 		Movie[] movies = { new Movie("에이리언 : 커버넌트", 15), new Movie("가디언즈 오브 갤럭시", 12) };
 		int age = 13;
 
-		for (Movie movie : movies) {
-			try {
+		try {
+			for (Movie movie : movies) {
 				movie.buyTicket(age);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				// e.printStackTrace();
 			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
